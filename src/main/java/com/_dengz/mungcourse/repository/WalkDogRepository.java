400e203f80e93c143a4a9d5b710b35171909d413
장현학\gusgk <gusgkr785@gmail.com>
2025-04-11 02:05:13 +0900

    hotfix: #6 - WalkDog 레포지토리도 추가

---
+++ src/main/java/com/_dengz/mungcourse/repository/WalkDogRepository.java
@@ -0,0 +1,7 @@
+package com._dengz.mungcourse.repository;
+
+import com._dengz.mungcourse.entity.WalkDog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WalkDogRepository extends JpaRepository<WalkDog, Long> {
+}
