cbe1ed4972ca90133ba0e4b8eddb6effbf7a5233
장현학\gusgk <gusgkr785@gmail.com>
2025-05-06 22:47:23 +0900

    hotfix : 회원탈퇴 디버깅 +

--- src/main/java/com/_dengz/mungcourse/service/AuthService.java
+++ src/main/java/com/_dengz/mungcourse/service/AuthService.java
@@ -52,12 +52,7 @@ public class AuthService {
 
     @Transactional
     public void deleteUser(String sub, String refreshToken) {
-        System.out.println(">>> 삭제할 사용자 sub: " + sub);
-        System.out.println(">>> 삭제 전 존재 확인: " + userRepository.existsBySub(sub));
-
         userRepository.deleteBySub(sub);
-
-        System.out.println(">>> 삭제 후 존재 확인: " + userRepository.existsBySub(sub));
-
+        logoutUser(refreshToken);
     }
 }
